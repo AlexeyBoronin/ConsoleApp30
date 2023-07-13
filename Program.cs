580edc0a1d13fd703a7dea4b043cb3c479098f40
Alexey <boronin_1993@mail.ru>
2023-07-13 21:17:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Mylib1;
+class StateConsumer
+{
+    public void PrintState()
+    {
+        DefaultState defaultState = new DefaultState();
+
+        InternalState internalState=new InternalState();
+
+        PublicState publicState = new PublicState();
+
+        publicState.PrintInternal();
+
+        publicState.PrintProtectedIntenal();
+
+        publicState.PrintPublic();
+    }
+}
